41b769ba40edd13d53838f7b4bd3239f79be813b
MASAM SANJANA <135332352+sanjanamasam1210@users.noreply.github.com>
2024-07-17 19:46:33 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,29 @@
+<?php
+// database connection code
+if(isset($_POST['txtName']))
+{
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('localhost', 'root', '','db_contact');
+
+// get the post records
+
+$txtName = $_POST['txtName'];
+$txtPhone = $_POST['txtPhone'];
+$txtMessage = $_POST['txtMessage'];
+
+// database insert SQL code
+$sql = "INSERT INTO tbl_contact (Id, fldName, fldPhone, fldMessage) VALUES ('0', '$txtName', '$txtPhone', '$txtMessage')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+}
+else
+{
+	echo "Are you a genuine visitor?";
+	
+}
+?>
